b36a8ac622f21e688b32cef6de508ad935715b97
Rio <rogerio.taxlien@gmail.com>
2020-01-15 10:26:32 -0700

    updating shared/index.html

--- web/shared/index.js
+++ web/shared/index.js
@@ -1,18 +1,18 @@
 <head>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
-function clicked(string) {
-    alert("CLICKED!!!");
-    if (string == "button1") {
-        document.getElementById("button1").innerHTML = "Button 1 clicked!";
-    }
-    if (string == "button2") {
-        document.getElementById("button2").innerHTML = "Button 2 clicked!";
-    }
-    if (string == "button3") {
-        document.getElementById("button3").innerHTML = "Button 3 clicked!";
-    }
- }
+// function clicked(string) {
+//     alert("CLICKED!!!");
+//     if (string == "button1") {
+//         document.getElementById("button1").innerHTML = "Button 1 clicked!";
+//     }
+//     if (string == "button2") {
+//         document.getElementById("button2").innerHTML = "Button 2 clicked!";
+//     }
+//     if (string == "button3") {
+//         document.getElementById("button3").innerHTML = "Button 3 clicked!";
+//     }
+// }
 // function changeColor() {
 //     newColor = document.getElementById("newColor").value;
 //     document.getElementById("div1").style.background = newColor;
