6aeb4fc3ff1da1dcd584e47079a3a0dc49b9a89c
Rio <rogerio.taxlien@gmail.com>
2020-01-14 19:58:37 -0700

    adding files from the team activity

---
+++ web/shared/index.js
@@ -0,0 +1,17 @@
+function clicked(string) {
+    alert("CLICKED!!!");
+    if (string == "button1") {
+        document.getElementById("button1").innerHTML = "Button 1 clicked!";
+    }
+    if (string == "button2") {
+        document.getElementById("button2").innerHTML = "Button 2 clicked!";
+    }
+    if (string == "button3") {
+        document.getElementById("button3").innerHTML = "Button 3 clicked!";
+    }
+}
+
+function changeColor() {
+    newColor = document.getElementById("newColor").value;
+    document.getElementById("div1").style.background = newColor;
+}
