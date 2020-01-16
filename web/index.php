721196d9d3aa24edce56d2c90ade8daa6ff4fb31
Rio <rogerio.taxlien@gmail.com>
2020-01-16 14:41:05 -0700

    updating landing page

--- web/index.php
+++ web/index.php
@@ -19,13 +19,13 @@
         <div class="col shadow centered buttonBox border" id="resume">Resume</div>
         <div class="col shadow centered buttonBox border" id="rideShare">Ride Share</div>
       </div>
-      <div class="container-fluid centered">
+      <div class="container-fluid centered row">
         <h1 class="p-3">Rio & Kristen</h1>
         <br>
         <img src="images/image1.jpg" alt="A picture of Rio and his wife Kristen" class="rounded img-resize-50-l">
       </div>
       <br>
-      <div class="container centered">
+      <div class="container-fluid centered row">
         <ul>
           <li><a href="hello.html">Hello World Page</a></li>
           <li><a href="shared/index.html">Team Activity Week 1</a></li>
