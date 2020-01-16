39d1113c26803567545ce73a5647864348838e9b
Rio <rogerio.taxlien@gmail.com>
2020-01-15 19:42:27 -0700

    changing the landing page

--- web/index.php
+++ web/index.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="first.css">
+        <title>Rogerio Moura</title>
+    </head>
+    <body>
+        <h1 class="centered">Hello! What assignment are you looking for?</h1>
+        <ul class="centered">
+            <li><a href="hello.html">Hello World Page</a></li>
+            <li><a href="shared/index.html">Team Activity Week 1</a></li>
+        </ul>
+    </body>
+</html>
+
+
 <?php
   echo "Hello World!";
   //phpinfo();
