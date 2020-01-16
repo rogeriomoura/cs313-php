230a4b0599f45ebd51a1b77272a3680b8adfce76
Rio <rogerio.taxlien@gmail.com>
2020-01-16 16:31:46 -0700

    updating landing page

---
+++ web/assignments.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="first.css">
+    <title>Assignments</title>
+</head>
+<body>
+    <div class="container-fluid headerBox centered">
+        <h1>Welcome to Rogerio Moura's Online Portfolio</h1>
+    </div>
+    <div class="container-fluid row centered">
+        <ul>
+          <li><a href="w1/hello.html">Hello World Page</a></li>
+          <li><a href="shared/index.html">Team Activity Week 1</a></li>
+        </ul>
+      </div>
+</body>
+</html>
