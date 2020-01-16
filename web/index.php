219a613e320abc6b44e9be399cf8b98aef609376
Rio <rogerio.taxlien@gmail.com>
2020-01-15 20:39:57 -0700

    updating landing page

--- web/index.php
+++ web/index.php
@@ -15,9 +15,9 @@
         <h1>Hello! What assignment are you looking for?</h1>
       </div>
       <div class="row">
-        <div class="col centered buttonBox border">Assignments</div>
-        <div class="col centered buttonBox border">Something</div>
-        <div class="col centered buttonBox border">Ride Share</div>
+        <div class="col shadow centered buttonBox border">Assignments</div>
+        <div class="col shadow centered buttonBox border">Something</div>
+        <div class="col shadow centered buttonBox border">Ride Share</div>
       </div>
       <div class="container centered">
         <ul>
