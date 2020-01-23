451d9a0326b4f5916123cd248fdd16aa7925fa57
Rio <rogerio.taxlien@gmail.com>
2020-01-23 16:02:29 -0700

    creating ride-share page

--- web/w3/thursdayClassTemp/thursdaySession1.php
+++ web/w3/thursdayClassTemp/thursdaySession1.php
@@ -12,6 +12,18 @@
          $_SESSION["favcolor"] = "green";
          $_SESSION["favanimal"] = "dolphin";
          // echo that variables have been set
+         if(!isset($_SESSION["favcolor"])) {
+            echo "Fav color has not been set.";
+         }
+         else {
+            echo "Fav color has been set to " . $_SESSION["favcolor"];
+         }
+         if(!isset($_SESSION["favanimal"])) {
+            echo "Fav animal has not been set.";
+         }
+         else {
+            echo "Fav color has been set to " . $_SESSION["favanimal"];
+         }
 ?>
       <a href="thursdaySession2.php">Check the variables on another page</a>
 
