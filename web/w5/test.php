6fc7b16292bc251723ba39fb50ff081c9a18f5b0
Rio <rogerio.taxlien@gmail.com>
2020-02-03 15:43:24 -0700

    changing ride-board app

--- web/w5/test.php
+++ web/w5/test.php
@@ -22,14 +22,24 @@ try{
     die();
 
 }
-
-
-foreach ($db->query('SELECT username, password FROM users') as $row)
-{
-  echo 'user: ' . $row['username'];
-  echo ' password: ' . $row['password'];
-  echo '<br/>';
-}
-
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Testing query with PHP</title>
+</head>
+<body>
+<?php
+    foreach ($db->query('SELECT username, password FROM users') as $row)
+    {
+      echo 'user: ' . $row['username'];
+      echo ' password: ' . $row['password'];
+      echo '<br/>';
+    }
+?>
+    
+</body>
+</html>
