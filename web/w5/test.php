1e0516741c400510edf27b6bcb981477f8aa469d
Rio <rogerio.taxlien@gmail.com>
2020-02-04 12:07:59 -0700

    trying to connect database

--- web/w5/test.php
+++ web/w5/test.php
@@ -9,35 +9,38 @@
 <body>
 <h1>This is the test querying data with PHP</h1>
 <?php 
-    try{
+    // try{
 
-        $dbUrl = getenv('DATABASE_URL');
+    //     $dbUrl = getenv('DATABASE_URL');
 
-        $dbOpts = parse_url($dbURL);
+    //     $dbOpts = parse_url($dbURL);
 
-        $dbHost = $dbOpts["host"];
-        echo "This is the dbHost: " . $dbHost . "<br>";
-        $dbPort = $dbOpts["port"];
-        echo "This is the dbPort: " . $dbPort . "<br>";
-        $dbUser = $dbOpts["user"];
-        echo "This is the dbUser: " . $dbUser . "<br>";
-        $dbPassword = $dbOpts["pass"];
-        echo "This is the dbPassword: " . $dbPassword . "<br>";
-        $dbName = ltrim($dbOpts["path"],'/');
-        echo "This is the dbName: " . $dbName . "<br>";
+    //     $dbHost = $dbOpts["host"];
+    //     echo "This is the dbHost: " . $dbHost . "<br>";
+    //     $dbPort = $dbOpts["port"];
+    //     echo "This is the dbPort: " . $dbPort . "<br>";
+    //     $dbUser = $dbOpts["user"];
+    //     echo "This is the dbUser: " . $dbUser . "<br>";
+    //     $dbPassword = $dbOpts["pass"];
+    //     echo "This is the dbPassword: " . $dbPassword . "<br>";
+    //     $dbName = ltrim($dbOpts["path"],'/');
+    //     echo "This is the dbName: " . $dbName . "<br>";
 
-        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    //     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        echo "<p>Database set up!</p><br>";
+    //     echo "<p>Database set up!</p><br>";
 
-    }catch(PDOException $ex){
+    // }catch(PDOException $ex){
 
-        echo 'Error!: ' . $ex->getMessage();
-        die();
+    //     echo 'Error!: ' . $ex->getMessage();
+    //     die();
 
-    }
+    // }
+
+    require "../dbConnect.php";
+    $db = get_db();
 
     foreach ($db->query('SELECT username, password FROM users') as $row)
     {
