562648517c612477befe504ecf3c68fd4413cc17
Rio <rogerio.taxlien@gmail.com>
2020-02-04 16:46:03 -0700

    W05 team activity

--- web/shared/w5team.php
+++ web/shared/w5team.php
@@ -23,28 +23,20 @@
     if(isset($_POST["submit"])) {
         echo "Inside IF statement";
         $bookName = $_POST["bookName"];
-        // $scriptures = $db->prepare("SELECT * FROM scriptures WHERE book = " . $_POST["bookName"]);
-        // $scriptures->execute();
+        $scriptures = $db->prepare("SELECT * FROM scriptures WHERE book = 'D&C'");
+        $scriptures->execute();
 
-        // echo "So far so Good!";
+        echo "So far so Good!";
     
-        // while ($fRow = $scriptures->fetch(PDO::FETCH_ASSOC)) {
-        //     $book = $fRow["book"];
-        //     $chapter = $fRow["chapter"];
-        //     $verse = $fRow["verse"];
-        //     $content = $fRow["content"];
+        while ($fRow = $scriptures->fetch(PDO::FETCH_ASSOC)) {
+            $book = $fRow["book"];
+            $chapter = $fRow["chapter"];
+            $verse = $fRow["verse"];
+            $content = $fRow["content"];
             
-        //     echo "<p>In <b>$book $chapter:$verse</b> it is written that, '$content'</p>";
-        // }
-
-        foreach ($db->query('SELECT * FROM scriptures WHERE book = ' . $bookName ) as $row)
-        {
-            echo 'book: ' . $row['book'];
-            echo 'verse : ' . $row['verse'];
-            echo '<br/>';
+            echo "<p>In <b>$book $chapter:$verse</b> it is written that, '$content'</p>";
         }
     }
-
 ?>
 </body>
 </html>
