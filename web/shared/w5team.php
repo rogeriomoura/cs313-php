5dd43dd3f9d8048e9d7bcd5479638d183f6317c7
Rio <rogerio.taxlien@gmail.com>
2020-02-04 16:30:09 -0700

    W05 team activity

--- web/shared/w5team.php
+++ web/shared/w5team.php
@@ -12,19 +12,29 @@
 </head>
 <body>
 <h1>Scripture Resources</h1>
-<?php 
-    $scriptures = $db->prepare("SELECT * FROM scriptures");
-    $scriptures->execute();
 
-    while ($fRow = $scriptures->fetch(PDO::FETCH_ASSOC)) {
-        $book = $fRow["book"];
-        $chapter = $fRow["chapter"];
-        $verse = $fRow["verse"];
-        $content = $fRow["content"];
-        
-        
-        echo "<p>In <b>$book $chapter:$verse</b> it is written that, '$content'</p>";
+<form method="post">
+    <input type="text" name="bookName">
+    <input type="submit" value="Submit">
+</form>
+
+<?php 
+    if(isset($_POST['submit'])) {
+        $bookName = $_POST["bookName"];
+        $scriptures = $db->prepare("SELECT * FROM scriptures WHERE book = $bookName");
+        $scriptures->execute();
+    
+        while ($fRow = $scriptures->fetch(PDO::FETCH_ASSOC)) {
+            $book = $fRow["book"];
+            $chapter = $fRow["chapter"];
+            $verse = $fRow["verse"];
+            $content = $fRow["content"];
+            
+            
+            echo "<p>In <b>$book $chapter:$verse</b> it is written that, '$content'</p>";
+        }
     }
+
 ?>
 </body>
 </html>
