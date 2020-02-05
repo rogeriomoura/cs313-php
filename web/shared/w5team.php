26ff6d52f5552aa63d701250757ddae88757aff8
Rio <rogerio.taxlien@gmail.com>
2020-02-04 17:59:47 -0700

    W05 team activity

--- web/shared/w5team.php
+++ web/shared/w5team.php
@@ -32,7 +32,7 @@
             // $content = $fRow["content"];
             $id = $fRow["id"];
             
-            echo "<p>In <b>$book $chapter:$verse</b> for details click here: </p><a href=\"/shared/w5details.php?id=$id\">Details</a>";
+            echo "<p>In <b>$book $chapter:$verse</b> for details click here: <a href=\"/shared/w5details.php?id=$id\">Details</a></p>";
         }
     }
 ?>
