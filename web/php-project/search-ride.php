0640de76b810d05b0da03601612394b84223cb66
Rio <rogerio.taxlien@gmail.com>
2020-02-07 18:14:34 -0700

    creating search-ride.php page

--- web/php-project/search-ride.php
+++ web/php-project/search-ride.php
@@ -10,7 +10,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
     <script src="reference.js"></script>
     <link rel="stylesheet" href="style.css">
-    <title>Offer a Ride</title>
+    <title>Search a Ride</title>
 </head>
 <body>
     <?php include "header.php"; ?>
@@ -20,62 +20,15 @@
             <form action="" method="post" class="needs-validation" novalidate>
                 <div class="form-row">
                     <div class="col">
-                        <label for="name">Name:</label><br>
-                        <input class="form-control" type="text" name="name" id="name" required placeholder="John Doe"><br>
+                        <label for="cityFrom">City From:</label><br>
+                        <input class="form-control" type="text" name="cityFrom" id="cityFrom" required placeholder="Rexburg"><br>
                     </div>
                     <div class="col">
-                        <label for="contact">Contact:</label><br>
-                        <input class="form-control" type="text" name="contact" id="contact" required placeholder="Phone number or Email"><br>
+                        <label for="cityTo">City To:</label><br>
+                        <input class="form-control" type="text" name="cityTo" id="cityTo" required placeholder="Salt Lake City"><br>
                     </div>
                 </div>
-                <div class="form-row">
-                    <div class="col">
-                        <label for="cFrom">City from:</label><br>
-                        <input class="form-control" type="text" name="cFrom" id="cFrom" required placeholder="Rexburg"><br>
-                    </div>
-                    <div class="col">
-                        <label for="cTo">City to:</label><br>
-                        <input class="form-control" type="text" name="cTo" id="cTo" required placeholder="Salt Lake"><br>
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="col">
-                        <label for="seats">Available seats:</label><br>
-                        <select class="form-control" name="seats" id="seats" required><br>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                        </select>
-                    </div>
-                    <div class="col">
-                        <label for="price">Price per person:</label><br>
-                        <input class="form-control" type="text" name="price" id="price" required placeholder="$20.00"><br>
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="col">
-                        <label for="date">Date:</label><br>
-                        <input class="form-control" type="date" min="<?=date('Y-m-d')?>"name="date" id="date" required><br>
-                    </div>
-                    <div class="col">
-                        <label for="time">Time of departure:</label><br>
-                        <select class="form-control" name="time" id="time" required><br>
-                            <option value="6a">06:00 AM</option>
-                            <option value="6:30a">06:30 AM</option>
-                            <option value="7a">07:00 AM</option>
-                            <option value="7:30a">07:30 AM</option>
-                            <option value="6p" selected>06:00 PM</option>
-                            <option value="6:30p">06:30 PM</option>
-                            <option value="7p">07:00 PM</option>
-                            <option value="7:30p">07:30 PM</option>
-                            <option value="tbd">TBD</option>
-                        </select>
-                    </div>
-                </div>
-                <input type="submit" value="Submit" class="btn btn-primary" id="submitOffer">
+                <input type="submit" value="Search">
             </form>
         </div>
     </div>
