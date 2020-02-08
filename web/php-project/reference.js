ec4e3b9a3467104bd37620b7f9a6652d8980deee
Rio <rogerio.taxlien@gmail.com>
2020-02-08 07:38:29 -0700

    final details in search-ride.php

--- web/php-project/reference.js
+++ web/php-project/reference.js
@@ -7,16 +7,16 @@ $(document).ready(function() { //or you can use $(function(){ jQuery goes here }
       window.location.href = "../php-project/search-ride.php";
     });
 
-    $("#backHomeBtn").click(function(){
-      window.location.href = "../php-project/ride-board.php";
-      alert("Back button clicked.");
-    });
-});
+    // $("#backHomeBtn").click(function(){
+    //   window.location.href = "../php-project/ride-board.php";
+    //   alert("Back button clicked.");
+    // });
 
-function backHomeBtn() {
-  alert("Back Home Button clicked!");
-  document.location.href = "../php-project/ride-board.php";
-}
+    function backHomeBtn() {
+      alert("Back Home Button clicked!");
+      document.location.href = "../php-project/ride-board.php";
+    }
+});
 
 //Validates the form for offer ride. If it's empty it stops the submit process
 (function() {
