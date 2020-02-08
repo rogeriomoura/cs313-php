11ff10779877e4d7ea70d4f2c3da9deee0014a93
Rio <rogerio.taxlien@gmail.com>
2020-02-08 07:33:45 -0700

    final details in search-ride.php

--- web/php-project/reference.js
+++ web/php-project/reference.js
@@ -14,6 +14,7 @@ $(document).ready(function() { //or you can use $(function(){ jQuery goes here }
 });
 
 function backHomeBtn() {
+  alert("Back Home Button clicked!");
   document.location.href = "../php-project/ride-board.php";
 }
 
