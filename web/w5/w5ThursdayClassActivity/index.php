1e5db5a4e0d0b5f6e66594dcc7736ca3af5cc889
Rio <rogerio.taxlien@gmail.com>
2020-02-11 08:35:31 -0700

    w5 Thursday page

--- web/w5/w5ThursdayClassActivity/index.php
+++ web/w5/w5ThursdayClassActivity/index.php
@@ -1,3 +1,34 @@
-<?php 
+<?php
+    require "../../dbConnect.php";
+    $db = get_db();
 
+    $events = $db->prepare("SELECT name, description, image FROM w5_event");
+    $events->execute();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>W5 events activity</title>
+</head>
+<body>
+    <h1>Events</h1><br>
+    <table>
+        <tr>
+            <td>Name</td>
+            <td>Description</td>
+            <td>Picture</td>
+        </tr>
+        <?php
+            while ($row = $events->fetch(PDO::FETCH_ASSOC)){
+                $name = $row["name"]; $description = $row["description"]; $image = $row["image"]; ?>
+                <tr>
+                    <td><?=$name?></td>
+                    <td><?=$description?></td>
+                    <td><a href="<?=$image?>"></a></td>
+                </tr>
+            <? }?>
+    </table>
+</body>
+</html>
