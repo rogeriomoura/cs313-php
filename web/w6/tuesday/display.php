ec271668a98bc0a797cec3f71effdce39d84b7b9
Rio <rogerio.taxlien@gmail.com>
2020-02-11 16:24:19 -0700

    w6 tuesday class

--- web/w6/tuesday/display.php
+++ web/w6/tuesday/display.php
@@ -8,7 +8,7 @@
             $personId = $_GET["personId"];
             echo $personId;
             $statement = $db->prepare('SELECT * FROM w6_user WHERE id = :personId');
-            echo "This is the statement: " . $statement;
+            // echo "This is the statement: " . $statement;
             $statement->bindValue(':personId', $personId);
             $statement->execute();
             while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
