40284fac5602b08af583d190e41d8cfd7e4e9e4d
Rio <rogerio.taxlien@gmail.com>
2020-02-11 18:39:15 -0700

    02-11-20

--- web/w6/teamActivity/insertScripture.php
+++ web/w6/teamActivity/insertScripture.php
@@ -6,13 +6,29 @@
     $chapter = $_POST['chapter'];
     $verse = $_POST['verse'];
     $content = $_POST['content'];
-    $topic = $_POST['topic'];
-    var_dump($topic);
+    $topics = $_POST['topic'];
+    //var_dump($topic);
     
     try 
     {
         $query = 'INSERT INTO scriptures (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)';	
-	    $statement = $db->prepare($query);
+        $statement = $db->prepare($query);
+        $statement->bindValue(':book', $book);
+        $statement->bindValue(':chapter', $chapter);
+        $statement->bindValue(':verse', $verse);
+        $statement->bindValue(':content', $content);
+        $statement->execute();
+
+        $scriptureId = $db->lastInsertId('scriptures_id_seq');
+
+        foreach ($topics as $topic){
+            $stQuery = 'INSERT INTO scripture_topics (scripture_id, topic_id)
+                        VALUES (:scriptureId, (SELECT id FROM topics WHERE name=\':topic\'))';
+            $stQuery = $db->prepare($stQuery);
+            $stQuery->bindValue(':scriptureId', $scriptureId);
+            $stQuery->bindValue(':topic', $topic);
+            $statement->execute();
+        }
     }
 
     catch (Exception $ex)
