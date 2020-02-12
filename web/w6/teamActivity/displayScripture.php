31a644cb6f69a0bca301b3eebae924d45964f5a1
Rio <rogerio.taxlien@gmail.com>
2020-02-12 16:03:12 -0700

    02-11-20

--- web/w6/teamActivity/displayScripture.php
+++ web/w6/teamActivity/displayScripture.php
@@ -2,15 +2,15 @@
     require("../../dbConnect.php");
     $db = get_db();
 
-    $query = 
+    $displayQuery = 
             "SELECT s.book, s.chapter, s.verse, s.content, t.name
             FROM scriptures s
             INNER JOIN scripture_topics st ON s.id = st.scripture_id
             INNER JOIN topics t ON t.id = st.topic_id
             ORDER BY t.name";
     
-    $statement = $db->prepare($query);
-    $statement->execute();
+    $displayStatement = $db->prepare($displayQuery);
+    $displayStatement->execute();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -28,16 +28,16 @@
             <td>Topics</td>
         </tr>
         <?php
-            while($row = $statement->fetch(PDO::FETCH_ASSOC)){
-                $book = $row["book"];
-                $chapter = $row["chapter"];
-                $verse = $row["verse"];
-                $content = $row["content"];
-                $topic = $row["name"]; ?>
+            while($dRow = $displayStatement->fetch(PDO::FETCH_ASSOC)){
+                $d_book = $dRow["book"];
+                $d_chapter = $dRow["chapter"];
+                $d_verse = $dRow["verse"];
+                $d_content = $dRow["content"];
+                $d_topic = $dRow["name"]; ?>
                 <tr>
-                    <td><?php echo "$book $chapter:$verse" ?></td>
-                    <td><?php echo "$content" ?></td>
-                    <td><?php echo "$topic" ?></td>
+                    <td><?php echo "$d_book $d_chapter:$d_verse" ?></td>
+                    <td><?php echo "$d_content" ?></td>
+                    <td><?php echo "$d_topic" ?></td>
                 </tr>
       <?php } ?>
     </table>
