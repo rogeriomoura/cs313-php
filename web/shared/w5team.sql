3548573efa3ea52232060b02058cfdc3787ef640
Rio <rogerio.taxlien@gmail.com>
2020-02-11 17:50:07 -0700

    02-11-20

--- web/shared/w5team.sql
+++ web/shared/w5team.sql
@@ -10,4 +10,3 @@ INSERT INTO scriptures (book, chapter, verse, content) VALUES('John', 1, 5, 'And
 INSERT INTO scriptures (book, chapter, verse, content) VALUES('D&C', 88, 49, 'The light shineth in darkness, and the darkness comprehendeth it not; nevertheless, the day shall come when you shall comprehend even God, being quickened in him and by him.');
 INSERT INTO scriptures (book, chapter, verse, content) VALUES('D&C', 93, 28, 'He that keepeth his commandments receiveth btruth and clight, until he is glorified in truth and knoweth all things.');
 INSERT INTO scriptures (book, chapter, verse, content) VALUES('Mosiah', 16, 9, 'He is the light and the life of the world; yea, a light that is endless, that can never be darkened; yea, and also a life which is endless, that there can be no more death.');
-
