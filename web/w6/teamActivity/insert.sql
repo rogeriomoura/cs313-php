5c11d1032a5eb9c43efe158487d4d92e5411a4e4
Rio <rogerio.taxlien@gmail.com>
2020-02-11 18:54:00 -0700

    02-11-20

--- web/w6/teamActivity/insert.sql
+++ web/w6/teamActivity/insert.sql
@@ -1,5 +1,6 @@
 DROP TABLE scripture_topics;
 DROP TABLE topics;
+DROP TABLE scriptures;
 
 CREATE TABLE topics
 (   id          SERIAL       NOT NULL PRIMARY KEY,
@@ -8,6 +9,18 @@ CREATE TABLE topics
 INSERT INTO topics (name) VALUES('Faith');
 INSERT INTO topics (name) VALUES('Sacrifice');
 INSERT INTO topics (name) VALUES('Charity');
+
+CREATE TABLE scriptures
+(   id          SERIAL       NOT NULL PRIMARY KEY,
+    book        VARCHAR(100) NOT NULL,
+    chapter     INT          NOT NULL,
+    verse       INT          NOT NULL,
+    content     VARCHAR(500) NOT NULL);
+
+INSERT INTO scriptures (book, chapter, verse, content) VALUES('John', 1, 5, 'And the light shineth in darkness; and the darkness comprehended it not.');
+INSERT INTO scriptures (book, chapter, verse, content) VALUES('D&C', 88, 49, 'The light shineth in darkness, and the darkness comprehendeth it not; nevertheless, the day shall come when you shall comprehend even God, being quickened in him and by him.');
+INSERT INTO scriptures (book, chapter, verse, content) VALUES('D&C', 93, 28, 'He that keepeth his commandments receiveth btruth and clight, until he is glorified in truth and knoweth all things.');
+INSERT INTO scriptures (book, chapter, verse, content) VALUES('Mosiah', 16, 9, 'He is the light and the life of the world; yea, a light that is endless, that can never be darkened; yea, and also a life which is endless, that there can be no more death.');
 
 CREATE TABLE scripture_topics
 (
