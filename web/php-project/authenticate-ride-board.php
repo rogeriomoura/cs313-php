4747e128adda8c3e989197c29ff1f3a1ed2765f6
Rio <rogerio.taxlien@gmail.com>
2020-02-13 16:20:01 -0700

    02-12-20

---
+++ web/php-project/authenticate-ride-board.php
@@ -0,0 +1,23 @@
+<?php
+    session_start();
+	require("dbConnect.php");
+	$db = get_db();
+
+    $personId = $_GET["personId"];
+    $statement = $db->prepare('SELECT * FROM w6_user WHERE id = :personId');
+    $statement->bindValue(':personId', $personId, PDO::PARAM_INT);
+    $statement->execute();
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
+        $id = $row["id"];
+        $first = $row["first_name"];
+        $last = $row["last_name"];
+        $food_id = $row["food_type"];
+
+        $fStatement = $db->prepare("SELECT * FROM w6_food WHERE id = $food_id");
+        $fStatement->execute();
+        while ($fRow = $fStatement->fetch(PDO::FETCH_ASSOC)){
+            $food = $fRow['food'];
+        }
+        echo "<h1>$first $last's favorite food is $food</h1>";
+    }
+    ?>
