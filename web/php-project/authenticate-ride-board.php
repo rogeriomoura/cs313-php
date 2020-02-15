f370d58e388b12d67375056a08f81709cbf056e8
Rio <rogerio.taxlien@gmail.com>
2020-02-14 18:39:11 -0700

    02-14-20

--- web/php-project/authenticate-ride-board.php
+++ web/php-project/authenticate-ride-board.php
@@ -10,28 +10,35 @@
     $username = htmlspecialchars($_POST["username"]);
     $password = htmlspecialchars($_POST["password"]);
 
-    $driver_statement = $db->prepare('SELECT id FROM user_driver WHERE username = :username AND password = :password');
-    $driver_statement->bindValue(':username', $username);
-    $driver_statement->bindValue(':password', $password);
-    $driver_statement->execute();
+    try {
+        $driver_statement = $db->prepare('SELECT id FROM user_driver WHERE username = :username AND password = :password');
+        $driver_statement->bindValue(':username', $username);
+        $driver_statement->bindValue(':password', $password);
+        $driver_statement->execute();
 
-    while ($dRow = $driver_statement->fetch(PDO::FETCH_ASSOC)){
-        $driver_id = $dRow["id"];
+        while ($dRow = $driver_statement->fetch(PDO::FETCH_ASSOC)){
+            $driver_id = $dRow["id"];
+        }
+
+        $rider_statement = $db->prepare('SELECT id FROM user_rider WHERE username = :username AND password = :password');
+        $rider_statement->bindValue(':username', $username);
+        $rider_statement->bindValue(':password', $password);
+        $rider_statement->execute();
+
+        while ($rRow = $rider_statement->fetch(PDO::FETCH_ASSOC)){
+            $rider_id = $rRow["id"];
+        }
+
+        if ($rider_id > 0 || $driver_id > 0 ){
+            $_SESSION["loggedIn"] = true;
+            $_SESSION["username"] = $username;
+        }
     }
-
-    $rider_statement = $db->prepare('SELECT id FROM user_rider WHERE username = :username AND password = :password');
-    $rider_statement->bindValue(':username', $username);
-    $rider_statement->bindValue(':password', $password);
-    $rider_statement->execute();
-
-    while ($rRow = $rider_statement->fetch(PDO::FETCH_ASSOC)){
-        $rider_id = $rRow["id"];
-    }
-
-    if ($rider_id > 0 || $driver_id > 0 ){
-        $_SESSION["loggedIn"] = true;
-        $_SESSION["username"] = $username;
+    catch (Exception $ex) {
+	    echo "Error with DB. Details: $ex";
+	    die();
     }
 
     header("Location: ride-board.php");
+    die();
 ?>
--- web/php-project/register-new-user.php
+++ web/php-project/register-new-user.php
@@ -8,35 +8,25 @@
         header("Location: ride-board.php");
     }
     else {
-        $username = htmlspecialchars($_POST["username"]);
-        $password = htmlspecialchars($_POST["password"]);
-        $contact = htmlspecialchars($_POST["contact"]);
-        $type = $_POST["type"];
-        var_dump($type);
-
-        // $driver_statement = $db->prepare('SELECT id FROM user_driver WHERE username = :username AND password = :password');
-        // $driver_statement->bindValue(':username', $username);
-        // $driver_statement->bindValue(':password', $password);
-        // $driver_statement->execute();
-
-        // while ($dRow = $driver_statement->fetch(PDO::FETCH_ASSOC)){
-        //     $driver_id = $dRow["id"];
-        // }
-
-        // $rider_statement = $db->prepare('SELECT id FROM user_rider WHERE username = :username AND password = :password');
-        // $rider_statement->bindValue(':username', $username);
-        // $rider_statement->bindValue(':password', $password);
-        // $rider_statement->execute();
-
-        // while ($rRow = $rider_statement->fetch(PDO::FETCH_ASSOC)){
-        //     $rider_id = $rRow["id"];
-        // }
-
-        // if ($rider_id > 0 || $driver_id > 0 ){
-        //     $_SESSION["loggedIn"] = true;
-        //     $_SESSION["username"] = $username;
-        // }
+        try {
+            $username = htmlspecialchars($_POST["username"]);
+            $password = htmlspecialchars($_POST["password"]);
+            $contact = htmlspecialchars($_POST["contact"]);
+            $type = $_POST["type"];
+            
+            $statement = $db->prepare('INSERT INTO user_:type (username, password, contact)
+                                    VALUES (:username, :password, :contact)');
+            $statement->bindValue(':username', $username);
+            $statement->bindValue(':password', $password);
+            $statement->bindValue(':contact', $contact);
+            $statement->execute();
+        }
+        catch (Exception $ex) {
+	        echo "Error with DB. Details: $ex";
+	        die();
+        }
         
-        // header("Location: ride-board.php");
+        header("Location: ride-board.php");
+        die();
     }
 ?>
