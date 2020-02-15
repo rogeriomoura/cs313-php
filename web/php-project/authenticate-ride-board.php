245ef1e353dc3bada2c4d1153a0e165078bad697
Rio <rogerio.taxlien@gmail.com>
2020-02-14 18:20:00 -0700

    02-14-20

--- web/php-project/authenticate-ride-board.php
+++ web/php-project/authenticate-ride-board.php
@@ -34,8 +34,6 @@
         $_SESSION["username"] = $username;
         $_SESSION["password"] = $password;
     }
-
-    // $message = "wrong answer";
-    // echo "<script type='text/javascript'>alert('$message');</script>";
+    
     header("Location: ride-board.php");
 ?>
