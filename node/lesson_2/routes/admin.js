bc8590318f737bbca050917e5aca04108e112da8
Rio <rogerio.taxlien@gmail.com>
2020-03-07 23:35:33 -0700

    lesson 2

---
+++ node/lesson_2/routes/admin.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const router = express.Router();
+
+const products = [];
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+});
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title, summary: req.body.summary });
+  console.log(products);
+  res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products; // exporting to the sop.js file to be displayed on the screen
